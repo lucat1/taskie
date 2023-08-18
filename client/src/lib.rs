6b99b219c41412df257e081b470a4350ab48a4db
Luca <luca.tagliavini5@studio.unibo.it>
2023-08-18 16:05:20 +0200

    initial client impl

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,14 +1,58 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use reqwest::StatusCode;
+use thiserror::Error;
+
+use taskie_structures::{CompleteTask, Execution, TaskKey};
+
+pub struct Client {
+    host: url::Url,
+    client: reqwest::Client,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Error, Debug)]
+pub enum ClientError {
+    #[error("Could not build the url for the action from the host parameter")]
+    ParseUrl(#[from] url::ParseError),
+    #[error("Error while sending HTTP request")]
+    Request(#[from] reqwest::Error),
+    #[error("Request failed with status code: {}", .0)]
+    Unsuccessful(StatusCode),
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Client {
+    pub fn new(host: url::Url) -> Self {
+        Client {
+            host,
+            client: reqwest::Client::new(),
+        }
+    }
+
+    pub async fn pop(&self) -> Result<Execution, ClientError> {
+        let pop_url = self.host.join("/v1/pop")?;
+        loop {
+            let response = self.client.get(pop_url.clone()).send().await;
+            match response {
+                Err(e) => {
+                    if !e.is_timeout() {
+                        return Err(e.into());
+                    }
+                }
+                Ok(response) => return Ok(response.json().await?),
+            }
+        }
+    }
+
+    pub async fn complete(&self, task_id: TaskKey) -> Result<(), ClientError> {
+        let complete_url = self.host.join("/v1/complete")?;
+        let response = self
+            .client
+            .post(complete_url.clone())
+            .json(&CompleteTask { id: task_id })
+            .send()
+            .await?;
+        if response.status().is_success() {
+            Ok(())
+        } else {
+            Err(ClientError::Unsuccessful(response.status()))
+        }
     }
 }
